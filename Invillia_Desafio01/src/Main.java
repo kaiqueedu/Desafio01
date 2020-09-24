75f90c7407e75ec9aaa78c523b878f510310c424
kaiqueedu <kaiqueedu94@gmail.com>
2020-09-24 11:46:14 -0300

    v2

--- Invillia_Desafio01/src/Main.java
+++ Invillia_Desafio01/src/Main.java
@@ -3,7 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("1, 2, 3, 4, 5, 6, 7, 8, 9 ou 10");
+		for(int i = 0; i <10; i++) {
+			System.out.print(i + ", ");
+		}
+		
+		System.out.print("ou 10");
 		
 	}
 }
